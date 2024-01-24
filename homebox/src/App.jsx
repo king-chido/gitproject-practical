aee6dabc3fc03dc4fa0901160057183fb5b91fed
kingsley <kingsleyibeh506@gamil.com>
2024-01-24 16:10:30 +0100

    two-c

--- homebox/src/App.jsx
+++ homebox/src/App.jsx
@@ -7,10 +7,22 @@ import Mainpage from "./component/mainpage/Mainpage";
 const App = () => {
   return (
     <main className="app-container">
-      <Header />
-      <Sidebar />
-      <Mainpage />
-      <Footer />
+      <div className="left-part">
+        <section className="side-section">
+          <Sidebar />
+        </section>
+      </div>
+      <div className="right-part">
+        <section className="header-section">
+          <Header />
+        </section>
+        <section className="mainpage-section">
+          <Mainpage />
+        </section>
+        <section className="footer-section">
+          <Footer />
+        </section>
+      </div>
     </main>
   );
 };
