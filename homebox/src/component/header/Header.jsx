69c25dc5399ceff25275299f7458bdea043459e1
myKey <divineobi1250@gmail.com>
2024-01-30 11:50:15 -0800

    modify

--- homebox/src/component/header/Header.jsx
+++ homebox/src/component/header/Header.jsx
@@ -1,8 +1,20 @@
 import "./header.css";
 
 const Header = () => {
-  return <div>Header
-    <button>ADD</button>
+  return <div className="main">
+    <div className="logo">
+      <h1 className="text">Fit Track</h1>
+    </div>
+    <div className="navs">
+      <nav className="nav">
+
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Login</li>
+        <li>Sign Up</li>
+      </nav>
+    </div>
   </div>;
 };
 
